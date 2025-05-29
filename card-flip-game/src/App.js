f41a50db024d2f3a055ad11dc241bb846bd150fb
mitrarsh <mitraroshanfekr05@gmail.com>
2025-05-29 12:43:59 +0330

    route

--- card-flip-game/src/App.js
+++ card-flip-game/src/App.js
@@ -2,12 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import './styles.css';
 import Board from './components/board';
+import { Routes,Route,Navigate} from 'react-router-dom';
+import Level2 from './components/level2';
+
 
 function App() {
   return (
     <div className="App">
       <h1>Flip the Cards!</h1>
-      <Board/>
+      <Routes>
+        <Route path="/level1" element={<Board/>} />
+        <Route path="/level2" element={<Level2 />} />
+      </Routes>
     </div>
   );
 }
